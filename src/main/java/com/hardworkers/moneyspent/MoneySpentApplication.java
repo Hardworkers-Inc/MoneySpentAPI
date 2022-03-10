e17256bfd09ae0b1d3be47e8ba1323eb500c2bcc
Ivanochko <vasa.ivanochko.17@gmail.com>
2022-03-10 17:06:19 +0200

    #1: Spring init
    (cherry picked from commit e1d28236c280cf34f89c8c32ba055091a899448c)

---
+++ src/main/java/com/hardworkers/moneyspent/MoneySpentApplication.java
@@ -0,0 +1,13 @@
+package com.hardworkers.moneyspent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneySpentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneySpentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hardworkers/moneyspent/MoneySpentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardworkers.moneyspent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneySpentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
